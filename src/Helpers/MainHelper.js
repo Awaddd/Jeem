485f53554cc3804e4d77477fee72b57b49f067a2
awaddd <awadini97@gmail.com>
2021-07-02 20:57:00 +0100

    now able to reflect calendar month progress changes on the calendar year component

--- src/Helpers/MainHelper.js
+++ src/Helpers/MainHelper.js
@@ -1,14 +1,21 @@
+import dayjs from 'dayjs';
+import objectSupport from 'dayjs/plugin/objectSupport';
+import { FORMAT_TIMESTAMP } from '../Helpers/DateHelper';
+
+dayjs.extend(objectSupport)
+
 export const initialiseCalendarMonth = (numWeeks, totalDays) => {
+
   let arr = [];
   let currentDay = 1;
 
   for (let week = 0; week < numWeeks; week++) {
-  
+
     arr[week] = [];
 
     for (let day = 0; day < 7; day++) {
       if (currentDay <= totalDays) {
-        arr[week][day] = {day: currentDay, active: false};
+        arr[week][day] = {day: currentDay, active: false, date: dayjs({ year: dayjs().year(), month: dayjs().month(), day: currentDay }).format(FORMAT_TIMESTAMP) };
         currentDay++;
       }
     }
@@ -16,4 +23,5 @@ export const initialiseCalendarMonth = (numWeeks, totalDays) => {
   }
 
   return arr;
+
 }
--- src/components/CalendarYear.jsx
+++ src/components/CalendarYear.jsx
@@ -12,33 +12,68 @@ function CalendarYear () {
 
   const [state, dispatch] = useContext(Context)
 
-
+  //
   let date = dayjs()
   let tertile = Math.floor((date.month() + 4) / 4) -1
   let currentTertile = [...months[tertile]]
 
+  //
   let year = date.year()
-  let month = currentTertile[0].key - 1
-  let dayOne = dayjs({year, month})
+  let firstDay = dayjs({year, month: currentTertile[0].key - 1})
 
-  month = currentTertile[3].key - 1
+  //
+  let month = currentTertile[3].key - 1
   let lastMonthInTertile = dayjs({year, month})
   let daysInMonth = dayjs(lastMonthInTertile).daysInMonth()
-  let lastDayInTertile = dayjs({year, month, day: daysInMonth})
+  let lastDay = dayjs({year, month, day: daysInMonth})
 
+  //
   let obj = {}
-  for (let i = dayOne.dayOfYear(); i <= lastDayInTertile.dayOfYear(); i++) {
-    obj[i] = { date: dayjs().dayOfYear(i).format(FORMAT_TIMESTAMP), active: false }
+  for (let i = firstDay.dayOfYear(); i <= lastDay.dayOfYear(); i++) {
+    obj[i] = { date: dayjs().dayOfYear(i).format('YYYY-MM-DD'), dayOfYear: i, active: false }
   }
 
+  //
   const [calendarTertile, setCalendarTertile] = useState(obj)
 
-  let x = Object.values(calendarTertile)
+  //
+  let calendarTertileArray = Object.values(calendarTertile)
+
+  //
+  useEffect(() => {
+
+    // flatten state.progress into just days instead of weeks/days
+    let progress = [].concat.apply([], state.progress)
+
+    if (progress.length > 0) {
+
+      // update current month's progress
+      let newArr = calendarTertileArray.map((day) => {
+
+        //
+        let today = dayjs().dayOfYear(dayjs(day.dayOfYear))
+
+        //
+        if (today.month() !== dayjs().month()) return day
+
+        //
+        if (today.month() == dayjs().month()) {
+          return progress[today.date() - 1]
+        }
+
+      })
+
+      //
+      setCalendarTertile(() => [...newArr])
+
+    }
+
+  }, [state.progress]);
 
   return (
     <div className="calendarYear">
-      {x.map((value, key) => (
-        <span className={value.active ? 'cell cell-activated' : 'cell'} key={key}></span>
+      {calendarTertileArray.map((day, key) => (
+        <span className={day.active ? 'cell cell-activated' : 'cell'} key={key}></span>
       ))}
     </div>
   )
